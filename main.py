c91469a46c6af3dbe2a24e3d285a2b21eb9cd402
bogdan01official <108180181+bogdan01official@users.noreply.github.com>
2023-11-29 21:06:21 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,81 @@
+import sqlite3
+
+# file work
+file = open("data.txt", "w")
+file.write("Hello, World")
+file.write("\nPython is awesome")
+file.write("\nI love programming")
+
+file.close()
+
+file = open("data.txt", "r")
+content_file = file.read()
+print(content_file)
+
+file.close()
+
+#SQLite
+#Table users
+def create_users():
+    conn = sqlite3.connect("mydatabase.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""CREATE TABLE users ( 
+        id INT PRIMARY KEY,
+        name TEXT,
+        email TEXT        
+    )""")
+    conn.close()
+
+def add_users():
+    conn = sqlite3.connect("mydatabase.db")
+    cursor = conn.cursor()
+
+    name = input("Введите name: ")
+    email = input("Введите email: ")
+
+    cursor.execute("INSERT INTO users (name, email) VALUES (?, ?)", (name, email))
+    conn.commit()
+    conn.close()
+
+try:
+    create_users()
+except:
+    print("уже существует")
+    add_users()
+
+
+
+#Table products
+def create_products():
+    conn = sqlite3.connect("mydatabase.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""CREATE TABLE products (
+        id INT PRIMARY KEY,
+        name TEXT,
+        price FLOAT,
+        quantity INT
+    )""")
+    conn.close()
+
+def add_products():
+    conn = sqlite3.connect("mydatabase.db")
+    cursor = conn.cursor()
+    products = [
+        (1, "Bread", 30.55, 60),
+        (2, "Oil", 90.89, 85),
+        (3, "Potato", 35.00, 70)
+    ]
+
+    for product in products:
+        cursor.execute("INSERT INTO products (id, name, price, quantity)"
+                       " VALUES (?, ?, ?, ?)", product)
+    conn.commit()
+    conn.close()
+
+try:
+    create_products()
+except:
+    print("Уже существует")
+    add_products()
